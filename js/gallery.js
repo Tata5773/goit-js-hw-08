ea26ae1c5a65e7e6258d0250a0e2b090c6ae7d88
Natalia Nemurka <124725617+Tata5773@users.noreply.github.com>
2024-11-05 22:21:28 +0200

    goit-js-hw-08

--- js/gallery.js
+++ js/gallery.js
@@ -69,16 +69,16 @@ const galleryContainer = document.querySelector(".gallery");
 const galleryMarkup = images
   .map(
     ({ preview, original, description }) => `
-    <li class="gallery-item">
-      <a class="gallery-link" href="${original}">
-        <img
-          class="gallery-image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-      </a>
-    </li>
+      <li class="gallery-item">
+          <a class="gallery-link" href="${original}">
+              <img
+                  class="gallery-image"
+                  src="${preview}"
+                  data-source="${original}"
+                  alt="${description}"
+              />
+          </a>
+      </li>
   `
   )
   .join("");
@@ -94,7 +94,7 @@ galleryContainer.addEventListener("click", (event) => {
 
   const instance = basicLightbox.create(`
     <img src="${originalSrc}" alt="${event.target.alt}" />
-    `);
+  `);
 
   instance.show();
 
